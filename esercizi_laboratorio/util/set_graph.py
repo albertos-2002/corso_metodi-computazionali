65eb154700ce57158e1ebfdaaffb60fd709c6771
Alberto <93335022+albertos-2002@users.noreply.github.com>
2024-10-19 23:50:43 +0200

    update

--- esercizi_laboratorio/util/set_graph.py
+++ esercizi_laboratorio/util/set_graph.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
 
-fontFlag   = True
-markerFlag = True
 
 def make_fine(axObj):
     
     #disegno degli assi
-    axObj.axhline( 0, color='grey', linestyle=(0, (1, 3)) ); # Horizontal axis
+    axObj.axhline( 0, color='grey', linestyle=(0, (1, 3)) );  # Horizontal axis
     axObj.axvline( 0, color='grey', linestyle=(0, (1, 3)) );  # Vertical axis
     
     axObj.legend()
@@ -15,29 +13,17 @@ def make_fine(axObj):
     return axObj
 
 
-def set_style(fontSize, markerSize):
+def set_style(fontSize=10, markerSize=6):
     
-    global fontFlag, markerFlag
+    plt.rcParams['axes.titlesize']  = fontSize;      # Set title size
+    plt.rcParams['axes.labelsize']  = fontSize+7;    # Set label size
+    plt.rcParams['legend.fontsize'] = fontSize-4
     
-    if fontSize == "auto":
-        fontFlag = False
-    
-    if markerSize == "auto":
-        markerFlag = False
-    
-    
-    #default 16
-    if fontFlag:
-        plt.rcParams['axes.titlesize']  = fontSize;  # Set title size
-        plt.rcParams['axes.labelsize']  = fontSize+5;    # Set label size
-        plt.rcParams['legend.fontsize'] = fontSize-4
-    
-    
-    if markerFlag:
-        plt.rcParams['lines.markersize'] = markerSize;  # Reset to the default value
+    plt.rcParams['lines.markersize'] = markerSize;  # Reset to the default value
     
     
     plt.tight_layout()
+    
     
     # Enable LaTeX rendering
     plt.rcParams['text.usetex'] = False

f780dd55f33fe5d113b549bd8fd3e5ee29769955
Alberto <93335022+albertos-2002@users.noreply.github.com>
2024-10-23 21:20:21 +0200

    update

--- esercizi_laboratorio/integrazione/integrazione.py
+++ esercizi_laboratorio/integrazione/integrazione.py
@@ -59,6 +59,11 @@ for numeroPunti in OrdiniDivisione:
     step = (estremoSuperiore - estremoInferiore) / numeroPunti
     puntiGriglia = np.arange( estremoInferiore, estremoSuperiore, step )
     
+    if len(puntiGriglia) % 2 != 0:
+        print("Numero di punti totali è dispari")
+        input()
+    
+    
     # RETTANGOLI NAIF -------------------------------------------------------
     #considera come valore di f in h l'estremo sinistro
     #ci fermiamo a n-1 elementi
@@ -126,11 +131,18 @@ for numeroPunti in OrdiniDivisione:
     # SIMPSON ---------------------------------------------------------------
     #consideriamo un polinomio di ordine 2 in un intervallo 2h
     sommaSimpson = 0
-    for index in range( 1, len(puntiGriglia)-1 ):
-        valoreFunzione_1 = np.e ** puntiGriglia[index-1] 
-        valoreFunzione_2 = 4 * (np.e ** puntiGriglia[index]) 
-        valoreFunzione_3 = np.e ** puntiGriglia[index+1] 
-        sommaSimpson += (valoreFunzione_1 + valoreFunzione_2 + valoreFunzione_3) * step / 3
+    for index in range( 1, len(puntiGriglia)-1 ): #non include l'ultimo punto e il primo delle ascisse
+        
+        if index % 2 == 0: #punti di indice pari
+            sommaSimpson += ( (2/3) * np.e ** puntiGriglia[index] ) * step
+        
+        else: #restanti punti di indice dispari
+            sommaSimpson += ( (4/3) * np.e ** puntiGriglia[index] ) * step
+        
+    
+    #punti di bordo
+    sommaSimpson += (1/3) * np.e ** puntiGriglia[1]
+    sommaSimpson += (1/3) * np.e ** puntiGriglia[-1]
     
     risultatiSimpson.append( sommaSimpson )
     errSimpson.append( abs(risultatoEsatto - sommaSimpson) )

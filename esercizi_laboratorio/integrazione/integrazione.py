26169b5c79b11ee9ebad009732073a697f7d8fab
Alberto <93335022+albertos-2002@users.noreply.github.com>
2024-10-25 19:12:07 +0200

    update

--- esercizi_laboratorio/integrazione/integrazione.py
+++ esercizi_laboratorio/integrazione/integrazione.py
@@ -19,7 +19,6 @@ import matplotlib.pyplot as plt
 
 import set_graph as sg
 
-debugFlag = True
 #global -----------------------------------------------------------
 OrdiniDivisione = []
 
@@ -39,160 +38,110 @@ errTrapezi        = []
 errSimpson        = [];  
 
 # ------------------------------------------
-numeroEsponenti = int( input("numero ordini di grandezza (10**) da spaziare: \n") )
+numeroEsponenti = int( input("numero ordini di grandezza (10**) del numero di intervalli: \n") )
 for ordine in range(1, numeroEsponenti+1, 1):
     OrdiniDivisione.append( 10**ordine )
-
-## -----------
-if debugFlag:
-    print("\n")
-    print("Esponenti per gli ordini N in cui su divide l'intervallo")
-    print(OrdiniDivisione)
-    print("\n")
-    input()
-
-## ------------
 
 
 for numeroPunti in OrdiniDivisione:
     
-    step = (estremoSuperiore - estremoInferiore) / numeroPunti
-    puntiGriglia = np.arange( estremoInferiore, estremoSuperiore, step )
+    #creazione della griglia di punti
+    puntiGriglia = np.linspace( estremoInferiore, estremoSuperiore, numeroPunti+1 )
+    step = puntiGriglia[1]-puntiGriglia[0]
+    """
+    Creiamo in questo modo una griglia equispaziata di 
+    N+1 punti (indici da 0 a N)
+    che suddivide l'intervallo di integrazione in N intervallini
+    """
     
-    if len(puntiGriglia) % 2 != 0:
-        print("Numero di punti totali è dispari")
-        input()
     
-    
-    # RETTANGOLI NAIF -------------------------------------------------------
+    # RETTANGOLI NAIF ----------------------------------------------------------------------------------------------------------------
     #considera come valore di f in h l'estremo sinistro
-    #ci fermiamo a n-1 elementi
+    """
+    len(puntiGriglia) non misura il numero di intervalli ma il numero di punti
+    che è N+1 (si considera anche il punto di indice zero)
+    per usare la notazione degli intervalli (primo punto x_0) usiamo la funzione range
+    dato che il punto di stop non è incluso
+    quindi inserendo nella funzione range (len(...) - 1) = ( N +1 -1) = N
+    quindi ci fermiano al punto N-1 (incluso) nella notazione della notazione degli intervalli  
+    """
     sommaRettangoliNaif = 0
-    for index in range( len(puntiGriglia) ):
-        valoreFunzione = np.e ** puntiGriglia[index]
-        sommaRettangoliNaif += valoreFunzione*step
+    for index in range( len(puntiGriglia)-1 ):
+        valoreFunzione = np.exp( puntiGriglia[index] )
+        sommaRettangoliNaif += valoreFunzione
+    
+    sommaRettangoliNaif *= step
     
     risultatiRettangoliNaif.append( sommaRettangoliNaif )
     errRettangoliNaif.append( abs(risultatoEsatto - sommaRettangoliNaif) )
-    ## -----------
-    if debugFlag:
-        print("\n")
-        print("Ordine attuale")
-        print(numeroPunti)
-        print("Errore rettangoli naif")
-        print( abs(risultatoEsatto - sommaRettangoliNaif) )
-        print("\n")
     
-    ## ------------
-    
-    # RETTANGOLI ------------------------------------------------------------
+    # RETTANGOLI ---------------------------------------------------------------------------------------------------------------------
     #consideriamo il valore centrale dell' intervallo h
-    #range fino a len-1, range restituisce argmax-1 e dobbia arrivare al penultimo punti considerando indice di lista
     sommaRettangoli = 0
     for index in range( len(puntiGriglia)-1 ):
         valoreEsponente = ( puntiGriglia[index+1] + puntiGriglia[index] ) /2
-        valoreFunzione = np.e ** valoreEsponente
-        sommaRettangoli += valoreFunzione*step
+        valoreFunzione = np.exp( valoreEsponente )
+        sommaRettangoli += valoreFunzione
+    
+    sommaRettangoli *= step
     
     risultatiRettangoli.append( sommaRettangoli )
     errRettangoli.append( abs(risultatoEsatto - sommaRettangoli) )
-    ## -----------
-    if debugFlag:
-        print("\n")
-        print("Ordine attuale")
-        print(numeroPunti)
-        print("Errore rettangoli")
-        print( abs(risultatoEsatto - sommaRettangoli) )
-        print("\n")
     
-    ## ------------
-    
-    # TRAPEZI ---------------------------------------------------------------
+    # TRAPEZI ------------------------------------------------------------------------------------------------------------------------
     #consideriamo la retta che passa per i due estremi dell'intervallo h
     sommaTrapezi = 0
     for index in range( len(puntiGriglia)-1 ):
-        valoreFunzione_1 = np.e ** puntiGriglia[index]
-        valoreFunzione_2 = np.e ** puntiGriglia[index+1]
-        sommaTrapezi += (valoreFunzione_1 + valoreFunzione_2) * step / 2
+        valoreFunzione_1 = np.exp( puntiGriglia[index] )
+        valoreFunzione_2 = np.exp( puntiGriglia[index+1] )
+        sommaTrapezi += (valoreFunzione_1 + valoreFunzione_2) / 2
+    
+    sommaTrapezi *= step
     
     risultatiTrapezi.append( sommaTrapezi )
     errTrapezi.append( abs(risultatoEsatto - sommaTrapezi) )
-    ## -----------
-    if debugFlag:
-        print("\n")
-        print("Ordine attuale")
-        print(numeroPunti)
-        print("Errore trapezi")
-        print( abs(risultatoEsatto - sommaTrapezi) )
-        print("\n")
     
-    ## ------------
-    
-    # SIMPSON ---------------------------------------------------------------
+    # SIMPSON ------------------------------------------------------------------------------------------------------------------------
     #consideriamo un polinomio di ordine 2 in un intervallo 2h
+    #Il numero di intervalli, perchè simpson funzioni, deve essere pari
+    # non è stato inserito un controllo, in quanto lo sono per costruzione
+    
     sommaSimpson = 0
-    for index in range( 1, len(puntiGriglia)-1 ): #non include l'ultimo punto e il primo delle ascisse
+    for index in range( 1, len(puntiGriglia)-1 ):
         
         if index % 2 == 0: #punti di indice pari
-            sommaSimpson += ( (2/3) * np.e ** puntiGriglia[index] ) * step
+            sommaSimpson += ( (2/3) * np.exp( puntiGriglia[index] ) )
         
         else: #restanti punti di indice dispari
-            sommaSimpson += ( (4/3) * np.e ** puntiGriglia[index] ) * step
+            sommaSimpson += ( (4/3) * np.exp( puntiGriglia[index] ) )
         
     
+    
     #punti di bordo
-    sommaSimpson += (1/3) * np.e ** puntiGriglia[1]
-    sommaSimpson += (1/3) * np.e ** puntiGriglia[-1]
+    sommaSimpson += ( (1/3) * np.exp( puntiGriglia[0] ) )
+    sommaSimpson += ( (1/3) * np.exp( puntiGriglia[-1] ) )
+    sommaSimpson *= step
     
     risultatiSimpson.append( sommaSimpson )
     errSimpson.append( abs(risultatoEsatto - sommaSimpson) )
-    ## -----------
-    if debugFlag:
-        print("\n")
-        print("Ordine attuale")
-        print(numeroPunti)
-        print("Errore simpson")
-        print( abs(risultatoEsatto - sommaSimpson) )
-        print("\n")
-        input()
-    
-    ## ------------
     
 
 
-## -----------
-if debugFlag:
-    print("\n")
-    input()
-    print("\n")
+# GRAFICA -------------------------------------------------------------------------------------------------------------------------
 
-## ------------
-
-# GRAFICA --------------------------------------------------
-
-figure , axs = plt.subplots(nrows=2, ncols=1)
-
+figure , ax = plt.subplots()
 sg.set_style(fontSize=19, markerSize=10)
 
-axs[0].set_title("Grafico errore (log)")
-axs[0].set_ylabel("$|\\ valoreEsatto - valoreCalcolato\\ |$")
-axs[0].set_xlabel("Ordini di grandezza")
-axs[0].semilogy()
-axs[0].semilogx()
-axs[0].plot( OrdiniDivisione, errRettangoliNaif, marker="x", linestyle=":", label=r'Rettangoli Naif $\mathcal{O}(h)$')
-axs[0].plot( OrdiniDivisione, errRettangoli,     marker="x", linestyle=":", label=r'Rettangoli $\mathcal{O}(h^2)$')
-axs[0].plot( OrdiniDivisione, errTrapezi,        marker="x", linestyle=":", label=r'Trapezi $\mathcal{O}(h^2)$')
-axs[0].plot( OrdiniDivisione, errSimpson,        marker="x", linestyle=":", label=r'Simpson $\mathcal{O}(h^4)$')
-sg.make_fine(axs[0])
-
-axs[1].set_title("Grafico errore")
-axs[1].set_ylabel("$|\\ valoreEsatto - valoreCalcolato\\ |$")
-axs[1].set_xlabel("Ordini di grandezza")
-axs[1].plot( OrdiniDivisione, errRettangoliNaif, marker="x", linestyle=":", label=r'Rettangoli Naif $\mathcal{O}(h)$')
-axs[1].plot( OrdiniDivisione, errRettangoli,     marker="x", linestyle=":", label=r'Rettangoli $\mathcal{O}(h^2)$')
-axs[1].plot( OrdiniDivisione, errTrapezi,        marker="x", linestyle=":", label=r'Trapezi $\mathcal{O}(h^2)$')
-axs[1].plot( OrdiniDivisione, errSimpson,        marker="x", linestyle=":", label=r'Simpson $\mathcal{O}(h^4)$')
-sg.make_fine(axs[1])
+ax.set_title("Grafico errore (log)")
+ax.set_ylabel("$|\\ valoreEsatto - valoreCalcolato\\ |$")
+ax.set_xlabel("Ordini di grandezza")
+ax.semilogy()
+ax.semilogx()
+ax.plot( OrdiniDivisione, errRettangoliNaif, marker="x", linestyle=":", label=r'Rettangoli Naif $\mathcal{O}(h)$')
+ax.plot( OrdiniDivisione, errRettangoli,     marker="x", linestyle=":", label=r'Rettangoli $\mathcal{O}(h^2)$')
+ax.plot( OrdiniDivisione, errTrapezi,        marker="x", linestyle=":", label=r'Trapezi $\mathcal{O}(h^2)$')
+ax.plot( OrdiniDivisione, errSimpson,        marker="x", linestyle=":", label=r'Simpson $\mathcal{O}(h^4)$')
+sg.make_fine(ax)
 
 plt.show()
 
